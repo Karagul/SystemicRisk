c5ceeaa824cc87b95c2fb364c00c94326c36e17c
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-01-31 22:05:53 +0100

    Added add liquidator and beginning of history recording option

--- BankNetwork.py
+++ BankNetwork.py
@@ -3,13 +3,25 @@ import numpy as np
 
 class BankNetwork:
 
-    def __init__(self, L, R, Q, alpha):
+    def __init__(self, L, R, Q, alpha, record=False):
         self.L = L
         self.R = R
         self.Q = Q
         self.alpha = alpha
+        self.record = record
         self.P = None
         self.E = None
+        if record:
+            self.Phist = []
+            self.Rhist = []
+            self.Ehist = []
+
+    def add_liquidator(self):
+        n = self.L.shape[0]
+        z = np.zeros((n, 1))
+        self.L = np.concatenate((z, self.L), axis=0)
+        z = np.zeros((1, n+1))
+        self.L = np.concatenate((z, self.L), axis=1)
 
     def get_loans(self):
         return np.sum(self.L, axis=0)
@@ -25,6 +37,9 @@ class BankNetwork:
         else:
             self.E = self.R + P + self.get_loans() - self.get_debts()
         self.P = P
+        if self.record:
+            self.Phist.append(self.P)
+            self.Ehist.append(self.E)
 
-    def update_equities(self):
+    def compute_equities(self):
         self.E = self.R + self.P + self.get_loans() - self.get_debts()
