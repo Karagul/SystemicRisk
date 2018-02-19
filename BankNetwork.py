81ae6b6581f6cacb408f19804a1819d3b2ddce70
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-02-19 17:55:59 +0100

    Added basic bricks for the dynamics

--- BankNetwork.py
+++ BankNetwork.py
@@ -1,15 +1,26 @@
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import normalize
 
 
 class BankNetwork:
 
-    def __init__(self, L, R, Q, alpha):
+    def __init__(self, L, R, Q, alpha, r, xi, zeta, bar_E=None):
         self.L = L
         self.R = R
         self.Q = Q
         self.alpha = alpha
         self.P = None
         self.E = None
+        if not bar_E:
+            self.bar_E = np.zeros((self.L.shape[0], ))
+        else:
+            self.bar_E = bar_E
+        self.Psi = None
+        self.Pi = None
+        self.r = r
+        self.xi = xi
+        self.zeta = zeta
         self.liquidator = False
         self.record = []
         self.period = 0
@@ -18,7 +29,7 @@ class BankNetwork:
         n = self.L.shape[0]
         z = np.zeros((n, 1))
         self.L = np.concatenate((z, self.L), axis=0)
-        z = np.zeros((1, n+1))
+        z = np.zeros((1, n + 1))
         self.L = np.concatenate((z, self.L), axis=1)
         self.liquidator = True
 
@@ -28,16 +39,24 @@ class BankNetwork:
     def get_debts(self):
         return np.sum(self.L, axis=1).T
 
-    def prices_update(self, X):
+    def get_defaulting(self):
+        return np.greater_equal(self.bar_E - self.E, 0).astype(np.int64)
+
+    def update_portfolios(self, X):
         P = np.dot(self.Q, X)
-        if self.E :
-            self.E -= self.P
-            self.E += P
-        else:
-            self.E = self.R + P + self.get_loans() - self.get_debts()
         self.P = P
 
-    def compute_equities(self):
+    def compute_psi(self):
+        self.Psi = normalize(self.L, axis=0, norm="l1")
+
+    def compute_pi(self):
+        self.Pi = self.xi * self.P + self.R + self.zeta * self.get_loans()
+
+    def update_reserves(self):
+        self.R += self.r * (self.get_loans() - self.get_debts()) + \
+            np.dot(self.Psi, self.Pi) * self.get_defaulting()
+
+    def update_equities(self):
         self.E = self.R + self.P + self.get_loans() - self.get_debts()
 
     def snap_record(self):
@@ -48,8 +67,3 @@ class BankNetwork:
         rec_dic["P"] = self.P.copy()
         rec_dic["E"] = self.E.copy()
         self.record.append(rec_dic)
-
-    def rebalance(self):
-
-    def default_check(self):
-
